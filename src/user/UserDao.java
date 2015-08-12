6b75c39097683b62d9b35508f10e89f76136b52d
gomgom <snut301@naver.com>
2015-08-13 04:12:10 +0900

    server-side haha!

---
+++ src/user/UserDao.java
@@ -0,0 +1,38 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserDao {
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://1.255.55.98:3306/Aeditor";
+
+	static final String USER = "root";
+	static final String PASS = "ra&eul";
+	
+	/**
+	 * 커넥션 공동 메소드
+	 * @returna
+	 * @throws ClassNotFoundException
+	 */
+	public Connection getConnection() throws ClassNotFoundException{
+		Connection dbConn = null;
+		
+		try{
+			Class.forName(JDBC_DRIVER);
+			dbConn = DriverManager.getConnection(DB_URL,USER,PASS);
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return dbConn;
+	}
+
+}
---
+++ src/user/UserServlet.java
@@ -0,0 +1,122 @@
+package user;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID=1L;
+	
+//	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.setContentType("application/json; charset=UTF-8");//READ
+//
+//		PrintWriter printout = response.getWriter();
+//		JSONObject JObject = new JSONObject();
+//		String type = request.getParameter("type");
+//
+//		UserDao dao = new UserDao();
+//
+//		try{
+//			if(type.equals("1")){	//User Info API
+//				String user_id=request.getParameter("id");
+//				
+//				JObject.put("result", dao.getUser(user_id));
+//			}else if(type.equals("2")){	//Login API
+//				
+//				String id = request.getParameter("id");
+//				String pass = request.getParameter("pass");
+//				
+//				JObject.put("result", dao.loginUser(id, pass));
+//			}else if(type.equals("3")){	//ID중복 API
+//				
+//				String check_id = request.getParameter("id");
+//				
+//				JObject.put("result", dao.checkUser(check_id));
+//			}
+//
+//
+//		}catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//		printout.print(JObject);
+//		printout.flush();
+//	}
+//
+//	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.setContentType("application/json; charset=UTF-8");//CREATE
+//
+//		PrintWriter printout = response.getWriter();
+//		JSONObject JObject = new JSONObject();
+//		String type = request.getParameter("type");
+//
+//		UserDao dao = new UserDao();
+////		Map<String, String[]> userParam = request.getParameterMap();
+//
+//		try{
+//			if(type.equals("1")){	//Sign in API
+//				Map<String, String[]> userParam = request.getParameterMap();				
+//				JObject.put("result", dao.postUser(userParam));
+//				
+//			}else if(type.equals("2")){	//Update API
+//
+//				String savePath = "/Users/gomi/workspace/AYH_NewDesign/WebContent/img/thumb";
+////				String savePath = "/Users/john/AYH_NewDesign/WebContent/img/thumb";
+//				int maxSize = 5*1024*1024;
+//				
+//				MultipartRequest multi = new MultipartRequest(request, savePath, maxSize, "UTF-8", new DefaultFileRenamePolicy());
+//			
+//				Map<String, String> userUpdateParam = new HashMap<String, String>();
+//				
+//				String thumbName=multi.getFilesystemName("thumb");
+//			
+//				userUpdateParam.put("id",multi.getParameter("id").toString());
+//				userUpdateParam.put("pass",multi.getParameter("pass").toString());
+//				userUpdateParam.put("name",multi.getParameter("name").toString());
+//				userUpdateParam.put("thumb",thumbName);
+//				
+//				JObject.put("result", dao.updateUserImg(userUpdateParam));
+//				
+//				}else if(type.equals("3")){
+//					Map<String, String[]> updateUserParam = request.getParameterMap();		
+//					JObject.put("result", dao.updateUser(updateUserParam));
+//				}
+//		}catch(JSONException e){
+//			
+//			e.printStackTrace();
+//		}
+//		printout.print(JObject);
+//		printout.flush();
+//	}
+//
+//	public void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.setContentType("application/json; charset=UTF-8");//DELETE
+//
+//		PrintWriter printout = response.getWriter();
+//		JSONObject JObject = new JSONObject();
+//
+//		UserDao dao = new UserDao();
+//		
+//		String userId = request.getParameter("id");
+//
+//		
+//		try{
+//			JObject.put("result", dao.deleteUser(userId));
+//		}catch(JSONException e){
+//			e.printStackTrace();
+//		}
+//		printout.print(JObject);
+//		printout.flush();
+//	}
+
+}
